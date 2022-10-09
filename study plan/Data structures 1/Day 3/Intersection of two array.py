2b4e9111d3151ff1c3d3227103389868b6878916
GalaxyNavy <113236785+GalaxyNavy@users.noreply.github.com>
2022-10-09 12:37:42 +0530

    Create Intersection of two array.py

---
+++ study plan/Data structures 1/Day 3/Intersection of two array.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+
+
+
+
+        c = Counter(nums1)
+        r = []
+        
+        for n in nums2:
+           
+            if c[n] > 0:
+                
+                r.append(n)
+                c[n] = c[n]-1
+        
+        return r
